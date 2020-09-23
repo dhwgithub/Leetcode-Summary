4bda92eb5e8a109a0a59d24f9591f289b7ee25c9
Hewen Deng <50670682+dhwgithub@users.noreply.github.com>
2020-09-23 19:52:21 +0800

    合并二叉树 - DFS

---
+++ One question per day/Merge_binary_tree.java
@@ -0,0 +1,31 @@
+/**
+给定两个二叉树，想象当你将它们中的一个覆盖到另一个上时，两个二叉树的一些节点便会重叠。
+你需要将他们合并为一个新的二叉树。合并的规则是如果两个节点重叠，那么将他们的值相加作为节点合并后的新值，否则不为 NULL 的节点将直接作为新二叉树的节点。
+注意: 合并必须从两个树的根节点开始。
+
+方法：对于节点，有至少一个为空情况，直接用另一个节点替换；否则，结果相加返回即可
+ */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Merge_binary_tree {
+    public TreeNode Merge_binary_tree(TreeNode t1, TreeNode t2) {
+        if (t1 == null) {
+            return t2;
+        }
+        if (t2 == null) {
+            return t1;
+        }
+        t1.val = t1.val + t2.val;
+        t1.left = mergeTrees(t1.left, t2.left);
+        t1.right = mergeTrees(t1.right, t2.right);
+        return t1;
+    }
+}
