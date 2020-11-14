f641854c23b1b88bfe76ec00c4657f7503f1a84b
Hewen Deng <50670682+dhwgithub@users.noreply.github.com>
2020-11-14 16:10:15 +0800

    奇偶链表 - 链表

---
+++ One question per day/Odd_Even_Linked_List.java
@@ -0,0 +1,44 @@
+/*
+给定一个单链表，把所有的奇数节点和偶数节点分别排在一起。请注意，这里的奇数节点和偶数节点指的是节点编号的奇偶性，而不是节点的值的奇偶性。
+请尝试使用原地算法完成。你的算法的空间复杂度应为 O(1)，时间复杂度应为 O(nodes)，nodes 为节点总数。
+
+说明:
+    应当保持奇数节点和偶数节点的相对顺序。
+    链表的第一个节点视为奇数节点，第二个节点视为偶数节点，以此类推。
+
+题解：
+	分别设置奇数节点和偶数节点，然后分别将其连接起来。注意保留偶数节点的头结点
+	最后奇数节点的最后一个节点连接偶数节点的头节点即可
+ */
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode evenHead = head.next;
+        ListNode odd = head;
+        ListNode even = evenHead;
+        while (even != null && even.next != null) {
+            odd.next = even.next;
+            odd = odd.next;
+
+            even.next = odd.next;
+            even = even.next;
+        }
+        odd.next = evenHead;
+
+        return head;
+    }
+}
